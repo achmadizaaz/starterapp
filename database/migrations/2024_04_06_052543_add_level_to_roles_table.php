cfa15990fad04366893d8a08e66487e445cd983c
achmadizaaz <myachmadizaaz@gmail.com>
2024-04-06 12:51:19 +0700

    Init

---
+++ database/migrations/2024_04_06_052543_add_level_to_roles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->integer('level')->nullable()->after('guard_name');
+            $table->boolean('is_admin')->default(0)->after('level');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->dropColumn('level');
+            $table->dropColumn('is_admin');
+        });
+    }
+};
